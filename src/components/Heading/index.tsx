127a2409279908b3ac30c8debb6806b10e02c3cf
NicholasNeto <nicholasneto@outlook.com>
2022-09-15 07:24:51 -0300

    nlw-mobile create component Heading

---
+++ src/components/Heading/index.tsx
@@ -0,0 +1,22 @@
+import { View, Text, ViewProps } from 'react-native';
+
+import { styles } from './styles';
+
+
+interface HeadingPropos extends ViewProps {
+    title: string;
+    subtitle: string;
+}
+
+export function Heading({ title, subtitle, ...rest }: HeadingPropos) {
+    return (
+        <View style={styles.container} {...rest}>
+            <Text style={styles.title}>
+                {title}
+            </Text>
+            <Text style={styles.subtitle}>
+                {subtitle}
+            </Text>
+        </View>
+    );
+}
